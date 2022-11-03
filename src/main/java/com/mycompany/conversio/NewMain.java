c1223e5bbd07177d129b8edf9f55ddd4469f99c2
danielwilfredo.amobe <danielwilfredo.amobe@203-10>
2022-11-03 15:51:46 +0100

    he cambiado el float del tipus de canv

---
+++ src/main/java/com/mycompany/conversio/NewMain.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.conversio;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author danielwilfredo.amobe
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Introdueix en €");
+        int valor_euros = sc.nextInt();
+                
+        double tipus_de_canvi = 0.87d;
+        System.out.println(valor_euros + " euros (€) actualmentt equivalen a " + (valor_euros * tipus_de_canvi) + " lliures esterlines (£)");
+    }
+    
+}
